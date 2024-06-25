4ffa999249fd0d440e88d172463afd86c1a0cea8
user name <email address>
2024-06-25 12:11:14 +0530

    test complete

---
+++ data.js
@@ -0,0 +1,10 @@
+let employee=[
+   
+    {id:101,name:"rahul",sal:4500},
+    {id:102,name:"rahulgandhi",sal:4500},
+    {id:103,name:"sonia",sal:4500}
+]
+function displayData(){
+    alert("GM")
+}
+
---
+++ three.js
@@ -0,0 +1,18 @@
+let emp={
+    eid:101,
+    ename:"Rahul",
+    esal:55000
+}
+let user={}
+
+console.log(Object.values(emp))
+console.log(Object.values(emp).length)
+console.log(Object.values(emp).length>0)
+
+
+if(Object.keys(user).length >0){
+    console.log("Not Empty Object")
+}
+else{
+    console.log("Empty Object")
+}
